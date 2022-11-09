45561d32f74364d2b9c5f552422e6647f775a46e
admin <1@ya.ru>
2022-11-09 20:02:36 +0300

    add Settings

--- core/base/settings/Settings.php
+++ core/base/settings/Settings.php
@@ -4,9 +4,51 @@ namespace core\base\settings;
 
 class Settings
 {
-    private  $routes = [
-        'admin' =>[
-            ''
+    static private $_instance;
+
+    private function __construct()
+    {
+    }
+
+    private function __clone()
+    {
+    }
+
+    static public function getInstance()
+    {
+        if (self::$_instance instanceof self) {
+            return self::$_instance;
+        }
+        return self::$_instance = new self;
+    }
+
+    private $routes = [
+        'admin' => [
+            'name' => 'admin',
+            'path' => 'core/admin/controller',
+            'hrUrl' => false
+        ],
+        'settings' => [
+            'path' => 'core/base/settings/'
+        ],
+        'plugins' => [
+            'path' => 'core/plugins',
+            'hrUrl' => false
+        ],
+        'user' => [
+            'path' => 'core/user/controller/',
+            'hrUrl' => true,
+            'routes' => []
+        ],
+        'default' => [
+            'controller' => 'IndexController',
+            'inputMethod' => 'inputData',
+            'outputMethod' => 'outputData'
         ]
-        ];
+    ];
+
+    static public function get($property)
+    {
+        return self::getInstance()->$property;
+    }
 }
